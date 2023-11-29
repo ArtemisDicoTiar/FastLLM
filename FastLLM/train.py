1660558a13986689c0ef01228e8ee201274575ff
jongyoonkim <johnjongyoonkim@gmail.com>
2023-11-29 19:10:59 +0900

    [#7] distillation method (tmp), scheduler (tmp)

--- FastLLM/train.py
+++ FastLLM/train.py
@@ -12,16 +12,32 @@ from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
 from FastLLM.constants import TARGET_MODEL_NAME, DATASET_NAME, DATASET_VERSION
 from FastLLM.models.base import Model
+from FastLLM.utils import distillation_loss
 
 if __name__ == '__main__':
     # ============= PARAMETERs ============= #
     device = 0
 
-    n_epochs = 1
-
-    learning_rate = 1e-3
-
     distil_method = NotImplemented
+
+    # both should be in [0, 1]
+    fixed_data_fraction = 0.1
+    drafter_data_fraction = 0.1
+
+    # https://arxiv.org/pdf/2310.08461.pdf
+    training_steps = 300_000
+    batch_size = 32
+
+    learning_rate = 3e-4
+    learning_rate_warmup_steps = 5_000
+    learning_rate_cooldown_step_start = 150_000
+    learning_rate_cooldown_step_end = 300_000
+
+    warmup_scheduler = "warmuplinear"
+    cooldown_scheduler = "cosine"
+
+    dropout = 0.0
+    n_epochs = 1
 
     # ============= DATASET ============= #
     dataset = load_dataset(DATASET_NAME, DATASET_VERSION, split="train")
@@ -42,20 +58,7 @@ if __name__ == '__main__':
     draft_model: Model = ...  # device=device
 
     # ============= LOSS FUNCTION (Distillation) ============= #
-    # https://arxiv.org/pdf/2310.08461.pdf
-    loss_fns = {
-        # Forward KL Divergence
-        "Seq_KD": NotImplemented,
-        # Forward KL Divergence
-        "Supervised_KD": NotImplemented,
-        # Forward KL Divergence
-        "Imit_KD": NotImplemented,
-        # Forward KL Divergence
-        "f_Distill": NotImplemented,
-        # Forward KL Divergence / Jensen-Shannon Divergence
-        "on_policy_GKD": NotImplemented,
-    }
-    loss_fn = loss_fns[distil_method]
+    loss_fn = distillation_loss.fns[distil_method]
 
     # ============= OPTIMIZER ============= #
     optimizer: AdamW = torch.optim.AdamW(
---
+++ FastLLM/utils/distillation_loss.py
@@ -0,0 +1,13 @@
+# https://arxiv.org/pdf/2310.08461.pdf
+fns = {
+    # Forward KL Divergence
+    "Seq_KD": NotImplemented,
+    # Forward KL Divergence
+    "Supervised_KD": NotImplemented,
+    # Forward KL Divergence
+    "Imit_KD": NotImplemented,
+    # Total Variation Distance
+    "f_Distill": NotImplemented,
+    # Forward KL Divergence / Jensen-Shannon Divergence
+    "on_policy_GKD": NotImplemented,
+}
