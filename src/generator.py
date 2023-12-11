061bb899540daae1cba0aa340424758f4b8c1546
Jaejin Kim <jaejin.kim@snu.ac.kr>
2023-12-11 16:53:46 +0900

    implement generator done!

--- src/generator.py
+++ src/generator.py
@@ -1,13 +1,58 @@
+from typing import Any
 from pydantic import BaseModel
-from transformers import (
-    AutoModelForSeq2SeqLM,
-    AutoTokenizer,
-    Text2TextGenerationPipeline,
-)
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, Pipeline
+from transformers.modeling_utils import PreTrainedModel
+from transformers.tokenization_utils import PreTrainedTokenizer
+
+from ..FastLLM.sampling.speculative_sampling import speculative_decoding
 
 
-class SpecDecText2TextGenerationPipeline(Text2TextGenerationPipeline):
-    pass
+class SpecDecText2TextGenerationPipeline(BaseModel):
+    drafter_model: PreTrainedModel
+    target_model: PreTrainedModel
+    tokenizer: PreTrainedTokenizer
+    device: int = -1
+
+    gamma: int = 5
+    temperature: float = 1.0
+    filter_thres: float = 0.9
+    lenience: float = 1.0
+    pad_id: int = 0
+    seq_len: int = 512
+
+    task: str = "text2text-generation"
+
+    def __init__(self, **data):
+        super().__init__(**data)
+
+        self.drafter_model.to(self.device)
+        self.target_model.to(self.device)
+
+    def __call__(
+        self,
+        input_string: str,
+    ) -> Any:
+        input_ids = self.tokenizer(input_string, return_tensors="pt").input_ids
+        input_ids = input_ids.to(self.device)
+
+        output_ids, accept_rate = speculative_decoding(
+            net=self.drafter_model,
+            small_net=self.target_model,
+            prompt=input_ids,
+            seq_len=self.seq_len,
+            gamma=self.gamma,
+            temperature=self.temperature,
+            filter_thres=self.filter_thres,
+            lenience=self.lenience,
+            pad_id=self.pad_id,
+        )
+
+        output_string = self.tokenizer.decode(output_ids[0], skip_special_tokens=True)
+
+        return {
+            "generated_text": output_string,
+            "generated_token_ids": output_ids[0],
+        }
 
 
 class Generator(BaseModel):
